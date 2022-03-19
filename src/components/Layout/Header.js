31570b3d6b7e4300c96bf79f50b672f30766a968
Christiansly <abiode23@gmail.com>
2022-03-19 09:22:28 +0100

    First Commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import mealImg from '../../assets/meals.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+function Header() {
+  return (
+   <>
+   <header className={classes.header}>
+       <h1>ReactMeals</h1>
+       <HeaderCartButton/>
+   </header>
+   <div className={classes["main-image"]}>
+       <img src={mealImg} alt="A table full of delicious food!"/>
+   </div>
+   </>
+  )
+}
+
+export default Header
