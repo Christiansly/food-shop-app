688b3754bc1c58d740accff6496a210865be1ece
Christiansly <abiode23@gmail.com>
2022-03-20 13:01:57 +0100

    Added Cart Close and Open Function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Fragment, useState } from 'react';
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 import Cart from './components/Cart/Cart'
+import CartProvider from './store/CartProvider'
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
   const showCartHandler = () => {
@@ -12,13 +13,13 @@ function App() {
     setCartIsShown(false)
   }
   return (
-    <Fragment>
-     {cartIsShown && <Cart />}
-    <Header />
+    <CartProvider>
+     {cartIsShown && <Cart onHideCart={hideCartHandler}/>}
+    <Header onShowCart={showCartHandler} />
     <main>
       <Meals />
     </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,12 @@ import React from 'react'
 import mealImg from '../../assets/meals.jpg'
 import classes from './Header.module.css'
 import HeaderCartButton from './HeaderCartButton'
-function Header() {
+function Header(props) {
   return (
    <>
    <header className={classes.header}>
        <h1>ReactMeals</h1>
-       <HeaderCartButton/>
+       <HeaderCartButton onClick={props.onShowCart}/>
    </header>
    <div className={classes["main-image"]}>
        <img src={mealImg} alt="A table full of delicious food!"/>
