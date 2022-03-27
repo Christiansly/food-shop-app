bbebe93ff45be8e653e1fc636418dd999d6d95c2
Christiansly <abiode23@gmail.com>
2022-03-27 10:44:37 +0100

    Added Cart Function Using Reducer and Context

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext }from "react";
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal'
+import CartContext from '../../store/cart-context'
 function Cart(props) {
+  const cartCtx = useContext(CartContext)
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
+  const hasItems = cartCtx.items.length > 0
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: "a1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+      {cartCtx.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
@@ -14,11 +19,11 @@ function Cart(props) {
       {cartItems}
       <div className={classes.total}>
           <span>Total Amount</span>
-          <span>35.62</span>
+          <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
       <div></div>
     </Modal>
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 function MealItemForm(props) {
+  const [amoutisValid, setAmoutisValid] = useState(true)
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    
+    const enteredAmount = amountInputRef.current.value;
+    console.log(enteredAmount)
+    const enteredAmountNumber = +enteredAmount;
+    console.log(enteredAmountNumber)
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmoutisValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber)
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -16,6 +38,7 @@ function MealItemForm(props) {
         }}
       />
       <button>+ Add</button>
+      {!amoutisValid && <p>Enter a valid value</p>}
     </form>
   );
 }
